b302dff1061a4a205df16845c2c30bd7b8648c11
Sudhanshu Sharma <sharmasudhanshu2110@gmail.com>
2020-05-03 09:53:35 +0530

    update

--- leetcode/main.py
+++ leetcode/main.py
@@ -1,34 +1,35 @@
-nums = 123456
-res = [int(x) for x in str(nums)]
-min_b = min(res)
+'''import heapq
+import itertools
+from itertools import product
+mat = [[1,3,11],[2,4,6]]
+k = 5
+#nums1= [ 1,  5,  9]
+#nums2 =[10, 11, 13]
+#nums3 = [12, 13, 15]
+#k =5 , key=sum)[:k])
+val= [mat[i] for i in range(len(mat))]
+val = [[j for j in range(len(mat))] for i in range(len(mat))]
+j = list(map(list, heapq.nsmallest(k+1,sorted(itertools.product(*mat)))))
+i = (sorted(list(product(*mat))))
+print(sum(j[k-1]))
+
+paths = [["B","C"],["D","B"],["C","A"]]
+for i,n in enumerate(paths):
+    #print(i)
+    for j,p in enumerate(n):
+        k = sum(p in item for item in paths)
+        if k ==1 & j ==1:
+            print(p)
 
 
-def replace(li: list, num1: int, num2: int):
-    for i, n in enumerate(li):
-        if li == num1:
-            li.remove(li)
-            print(li)
-            li.insert(n, num2)
-    return li
 
-
-new_list = replace(res, min_b, 0)
-
-
-
-
-
-
-
-'''res_min = res[0:len(res)//2]
-min_b = min(i for i in res_min if i > 0)
-max_a = max(res_min)
-#[sub.replace('4', '1') for sub in test_list]
-if min_b==res[0]:
-    min_val = 0
-    repalce_val = min()
-    rep = [sub.replace(repalce_val,min_val) for sub in res]
-else
-'''
-
-
+J = "z"
+S = "ZZ"
+K =0
+j = list(J)
+s = list(S)
+for i in S:
+    k =  sum(i in item for item in J)
+    K +=k
+print(K)
+'''''
